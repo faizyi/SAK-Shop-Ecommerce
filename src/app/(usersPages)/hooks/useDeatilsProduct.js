1412b1fc535035370d1adb6737c354233a012252
faizyi <faizi13062000@gmail.com>
2024-11-03 21:40:23 +0500

    logs del

--- src/app/(usersPages)/hooks/useDeatilsProduct.js
+++ src/app/(usersPages)/hooks/useDeatilsProduct.js
@@ -8,7 +8,6 @@ export default function useDeatilsProduct(productId){
         const fetchProduct = async()=>{
             const res = await getProductDetailsById(productId);
             if(res.status === 200){
-            console.log(res.data.data)
             setDetailProduct(res.data.data);
             }
         }
