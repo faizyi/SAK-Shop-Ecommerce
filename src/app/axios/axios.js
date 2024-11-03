70b241a711e5ef4b6829fc07848d05d3217c87b6
faizyi <faizi13062000@gmail.com>
2024-11-03 19:53:46 +0500

    url change

--- src/app/axios/axios.js
+++ src/app/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://sakbackend-p4jg46vrg-faizyis-projects.vercel.app",
+    baseURL: "https://sakbackend.vercel.app/",
     headers: {
         "Content-Type": "application/json",
     },
